1e8bd9e63fddf11d38aebedd5a42853499eedbbf
bhamann-collab <brockohamann@gmail.com>
2020-08-01 14:07:35 +0930

    Added react code

--- back-end/server.js
+++ back-end/server.js
@@ -1,16 +1,22 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
-//const passport = require('passport');
-//const passportLocal = require('passport-local').Strategy;
+const passport = require('passport');
+const passportLocal = require('passport-local').Strategy;
+const cookieParser = require("cookie-parser");
+const bcrype = require('bcryptjs');
 const session = require('express-session');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}))
 
 app.use(session({
     secret: "secretcode",
@@ -18,8 +24,20 @@ app.use(session({
     saveUninitialized: true
 }))
 
+app.use(cookieParser("secretcode"))
+//routes
+app.post("/login", (req, res) => {
+    console.log(req.body);
+})
+app.post("/register", (req, res) => {
+    console.log(req.body)
+})
+app.get('/user', (req, res) => {
+    
+})
 app.get('/', (req, res) => {
-    res.send('hello')
+    console.log(req.session)
+    res.send(`hello ${req.session.views}`)
 })
 
 app.listen(4000, () => {
